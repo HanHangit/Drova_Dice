33bf58077e075610c6f130e1570b65f3cf3abf55
12Cy <christian.sandkaemper@gmail.com>
2020-11-14 00:27:39 +0100

    #Add basics gui

---
+++ Assets/Scripts/GUI_Health.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GUI_Health : MonoBehaviour
+{
+	[SerializeField]
+	private TextMeshProUGUI _healthText = default;
+
+	public void Init()
+	{
+
+	}
+
+	public void ChangeHealth()
+	{
+
+	}
+}
