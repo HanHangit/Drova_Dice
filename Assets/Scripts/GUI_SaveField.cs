38c0ba9727bacc30152c5083dfb89ba37ea89330
12Cy <christian.sandkaemper@gmail.com>
2020-11-14 17:34:36 +0100

    asd

--- Assets/Scripts/GUI_SaveField.cs
+++ Assets/Scripts/GUI_SaveField.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using DrovaDiceLogic;
 using DrovaDiceLogic.BoardLogic;
 using DrovaDiceLogic.Moves;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -25,6 +26,14 @@ public class GUI_SaveField : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 
 	private void ActionChangedListener(DiceGame.GameTurnEndedEventArgs args)
 	{
+		for (int i = _diceNumbers.Count - 1 ; i >= 0 ; i--)
+		{
+			var instance = _diceNumbers[i];
+			_diceNumbers.Remove(instance);
+			Destroy(instance.gameObject);
+		}
+
+
 		var dices = GameManager.Instance.GetCurrentGame().CurrentBoard.Dices;
 		for (int i = 0; i < dices.Count; i++)
 		{
@@ -52,7 +61,7 @@ public class GUI_SaveField : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 	public void OnPointerClick(PointerEventData eventData)
 	{
 		var game = GameManager.Instance.GetCurrentGame();
-		var action = new SaveAction(null);
+		var action = new SaveAction();
 		if (game.CanBePlayed(action))
 		{
 			game.Play(action);
